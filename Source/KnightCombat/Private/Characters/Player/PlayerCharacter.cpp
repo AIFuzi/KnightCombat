e34c86fe96426e15e359b0d4fc3aa1bc31a74da4
AIFuzi <39236252+AIFuzi@users.noreply.github.com>
2022-08-27 23:45:07 +0300

    Player base input

--- Source/KnightCombat/Private/Characters/Player/PlayerCharacter.cpp
+++ Source/KnightCombat/Private/Characters/Player/PlayerCharacter.cpp
@@ -1,10 +1,28 @@
 #include "Characters/Player/PlayerCharacter.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+APlayerCharacter::APlayerCharacter()
+{
+	bUseControllerRotationYaw = false;
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	
+	SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArmComponent->SetupAttachment(GetMesh());
+
+	CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("PlayerCamera"));
+	CameraComponent->SetupAttachment(SpringArmComponent);
+}
 
 void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
-	
+	PlayerInputComponent->BindAxis("MoveForward", this, &APlayerCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &APlayerCharacter::MoveRight);
+	PlayerInputComponent->BindAxis("Turn", this, &APlayerCharacter::Turn);
+	PlayerInputComponent->BindAxis("LookUp", this, &APlayerCharacter::LookUp);
 }
 
 void APlayerCharacter::MoveForward(float Value)
@@ -27,4 +45,14 @@ void APlayerCharacter::MoveRight(float Value)
 
 		AddMovementInput(Direction, Value);
 	}
+}
+
+void APlayerCharacter::LookUp(float Value)
+{
+	AddControllerPitchInput(Value * MouseSensitivity);
+}
+
+void APlayerCharacter::Turn(float Value)
+{
+	AddControllerYawInput(Value * MouseSensitivity);
 }
--- Source/KnightCombat/Public/Characters/Player/PlayerCharacter.h
+++ Source/KnightCombat/Public/Characters/Player/PlayerCharacter.h
@@ -11,11 +11,25 @@ class KNIGHTCOMBAT_API APlayerCharacter : public ABaseCharacter
 
 public:
 
+	APlayerCharacter();
+
 	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
 
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Character")
+	class USpringArmComponent* SpringArmComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Character")
+	class UCameraComponent* CameraComponent;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Params")
+	float MouseSensitivity = 1.f;
+	
 private:
 
 	void MoveForward(float Value);
-	void MoveRight(float Right);
+	void MoveRight(float Value);
+
+	void LookUp(float Value);
+	void Turn(float Value);
 	
 };
