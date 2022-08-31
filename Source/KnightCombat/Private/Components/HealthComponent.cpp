af40937c4ce1e291acf975840194de8ae9f17984
AIFuzi <39236252+AIFuzi@users.noreply.github.com>
2022-08-31 16:43:52 +0300

    Stamina use value

--- Source/KnightCombat/Private/Components/HealthComponent.cpp
+++ Source/KnightCombat/Private/Components/HealthComponent.cpp
@@ -14,6 +14,7 @@ void UHealthComponent::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& Out
 	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
 
 	DOREPLIFETIME(UHealthComponent, CurrentHealth);
+	DOREPLIFETIME(UHealthComponent, CurrentStamina);
 }
 
 void UHealthComponent::GetDamage(float Damage)
@@ -59,3 +60,63 @@ void UHealthComponent::Server_RegenHealth_Implementation()
 }
 
 bool UHealthComponent::Server_RegenHealth_Validate() { return true; }
+
+void UHealthComponent::StartUseStamina()
+{
+//	GetWorld()->GetTimerManager().SetTimer()
+}
+
+void UHealthComponent::StopUseStamina()
+{
+	
+}
+
+void UHealthComponent::UseStaminaValue(float Value)
+{
+	Server_UseStaminaValue(Value);
+}
+
+void UHealthComponent::Server_UseStaminaValue_Implementation(float Value)
+{
+	if(GetOwnerRole() == ROLE_Authority)
+	{
+		if(CurrentStamina >= Value)
+		{
+			StopRegenStamina();
+			
+			CurrentStamina = CurrentStamina - Value;
+			CurrentStamina = FMath::Max(CurrentStamina, 0.f);
+
+			StartRegenStamina();
+		}
+	}
+}
+
+bool UHealthComponent::Server_UseStaminaValue_Validate(float Value) { return true; }
+
+void UHealthComponent::StartRegenStamina()
+{
+	GetWorld()->GetTimerManager().SetTimer(RegenStaminaTimer, this, &UHealthComponent::Server_RegenStamina, RegenStaminaRate, true, RegenStaminaStartDelay);
+}
+
+void UHealthComponent::StopRegenStamina()
+{
+	GetWorld()->GetTimerManager().ClearTimer(RegenStaminaTimer);
+}
+
+void UHealthComponent::Server_RegenStamina_Implementation()
+{
+	if(GetOwnerRole() == ROLE_Authority)
+	{
+		
+	}
+}
+
+bool UHealthComponent::Server_RegenStamina_Validate() { return true; }
+
+void UHealthComponent::Server_UseStamina_Implementation()
+{
+	
+}
+
+bool UHealthComponent::Server_UseStamina_Validate() { return true; }
--- Source/KnightCombat/Public/Components/HealthComponent.h
+++ Source/KnightCombat/Public/Components/HealthComponent.h
@@ -5,6 +5,7 @@
 #include "HealthComponent.generated.h"
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE(FHealthEnded);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FStaminaEnded);
 
 UCLASS(Blueprintable, BlueprintType, ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class KNIGHTCOMBAT_API UHealthComponent : public UActorComponent
@@ -36,6 +37,24 @@ public:
 	UPROPERTY(BlueprintAssignable, Category="Health")
 	FHealthEnded OnHealthEnded;
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Stamina")
+	float MaxStamina;
+
+	UPROPERTY(Replicated, EditAnywhere, BlueprintReadWrite, Category="Stamina")
+	float CurrentStamina;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Stamina")
+	float RegenStaminaRate;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Stamina")
+	float SprintStaminaRate;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Stamina")
+	float RegenStaminaStartDelay;
+
+	UPROPERTY(BlueprintAssignable, Category="Stamina")
+	FHealthEnded OnStaminaEnded;
+
 	UFUNCTION(BlueprintCallable, Category="Health")
 	void GetDamage(float Damage);
 
@@ -45,15 +64,39 @@ public:
 	UFUNCTION(BlueprintCallable, Category="Health")
 	void StopRegenHealth();
 
+	UFUNCTION(BlueprintCallable, Category="Stamina")
+	void StartUseStamina();
+
+	UFUNCTION(BlueprintCallable, Category="Stamina")
+	void StopUseStamina();
+
+	UFUNCTION(BlueprintCallable, Category="Stamina")
+	void UseStaminaValue(float Value);
+
+	UFUNCTION(BlueprintCallable, Category="Stamina")
+	void StartRegenStamina();
+
+	UFUNCTION(BlueprintCallable, Category="Stamina")
+	void StopRegenStamina();
+
 private:
 
 	FTimerHandle RegenHealthTimer;
 	FTimerHandle RegenStaminaTimer;
+	FTimerHandle UseStaminaTimer;
 
 	UFUNCTION(Unreliable, Server, WithValidation, Category="Health")
 	void Server_GetDamage(float Damage);
 
 	UFUNCTION(Unreliable, Server, WithValidation, Category="Health")
 	void Server_RegenHealth();
-		
+
+	UFUNCTION(Unreliable, Server, WithValidation, Category="Stamina")
+	void Server_UseStamina();
+
+	UFUNCTION(Unreliable, Server, WithValidation, Category="Stamina")
+	void Server_RegenStamina();
+
+	UFUNCTION(Unreliable, Server, WithValidation, Category="Stamina")
+	void Server_UseStaminaValue(float Value);
 };
